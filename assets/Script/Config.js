c175707bfbdc7a6ac4c9d110038fb123c03b3b73
chenyomi <408550179@qq.com>
2018-03-27 09:02:06 +0800

    提交更新

---
+++ assets/Script/Config.js
@@ -0,0 +1,19 @@
+window.Config = {
+  shopP2P: 1, //交易市场场景切换参数
+  apiUrl: "http://www.jingongbao.com:4633",
+  backUrl: "userCenter",
+  openID: null,
+  //兑换物品的数据
+  newSocket: null,
+  exchangeData: {
+    actualName: null,
+    virtualName: null,
+    actualCount: null,
+    virtualCount: null,
+    goodsType: null
+  },
+  addressId: 0, //地址ID，
+  firstLogin: false,
+  messageCount: 0,
+  addressId: 0 //地址ID
+};
