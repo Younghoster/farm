a871debd57a0a5231e7524c8bf07b32c1f39ab89
shockingsrose <847073376@qq.com>
2018-04-04 15:17:58 +0800

    1.小鸡详情页（小鸡数据绑定  生长周期和转手记录后台还没有 ） 2.日历从签到中分离（为了在生长周期中使用）

---
+++ assets/Script/Menu_Modal/signIn.js
@@ -0,0 +1,58 @@
+var Data = require("Data");
+var Func = Data.func;
+cc.Class({
+  extends: cc.Component,
+
+  properties: {},
+
+  // 关闭模态框
+  closeModal() {
+    var self = this;
+
+    var action = cc.fadeOut(0.3);
+    this.node.runAction(action);
+    setTimeout(() => {
+      this.node.active = false;
+    }, 400);
+
+    // scrollView.removeFromParent();
+    // this.node.removeChild(Modal);
+  },
+
+  signIn() {
+    this.canlendarJs.todayNode.getChildByName("item_do").active = true;
+    this.canlendarJs.todayNode.getChildByName("item_undo").active = false;
+    Func.PostSign().then(data => {
+      if (data.Code === 1) {
+        var signButton = cc.find("bg/btn-sign", this.node);
+        cc.loader.loadRes("btn-hasSign", cc.SpriteFrame, function(err, spriteFrame) {
+          signButton.getComponent(cc.Sprite).spriteFrame = spriteFrame;
+        });
+        Msg.show("签到成功! 积分增加" + data.Point + ",牧场币增加" + data.RachMoney, 0.3, 3000);
+      }
+    });
+  },
+
+  onLoad() {
+    this.calendarNode = cc.find("bg/calendar", this.node);
+    this.canlendarJs = this.calendarNode.getComponent("Calendar");
+  },
+
+  start() {
+    Func.GetSignList().then(data => {
+      if (data.Code === 1) {
+        this.canlendarJs.func.initCalendar.call(this.canlendarJs, data.List);
+        //已签到 按钮变灰
+        if (this.canlendarJs.todayNode.getChildByName("item_do").active) {
+          var signButton = cc.find("bg/btn-sign", this.node);
+          cc.loader.loadRes("btn-hasSign", cc.SpriteFrame, function(err, spriteFrame) {
+            signButton.getComponent(cc.Sprite).spriteFrame = spriteFrame;
+            signButton.getComponent(cc.Button).interactable = false;
+          });
+        }
+      } else {
+        Msg.show(data.Message);
+      }
+    });
+  }
+});
--- assets/Script/chickDetail.js
+++ assets/Script/chickDetail.js
@@ -15,7 +15,7 @@ cc.Class({
     this.growNode = cc.find("bg-f3/bg/grow/progressBar", this.node);
     this.growProgressBar = cc.find("bg-f3/bg/grow/progressBar", this.node).getComponent(cc.ProgressBar);
     this.growBar = cc.find("bg-f3/bg/grow/progressBar/bar", this.node);
-    this.growLabel = cc.find("bg-f3/bg/grow/value", this.node);
+    this.growLabel = cc.find("bg-f3/bg/grow/value", this.node).getComponent(cc.Label);
 
     this.growthButton = cc.find("bg-f3/btn-group/btn-growth", this.node);
     this.growthLabelNode = cc.find("bg-f3/btn-group/btn-growth/label", this.node);
@@ -23,14 +23,20 @@ cc.Class({
     this.recordButton = cc.find("bg-f3/btn-group/btn-record", this.node);
     this.recordLineNode = cc.find("line", this.recordButton);
     this.recordLabelNode = cc.find("bg-f3/btn-group/btn-record/label", this.node);
-
+    // chickList滚动视图 内容节点
+    this.contentNode = cc.find("chickList/view/content", this.node);
+    //Tab切换的内容
     this.content1Node = cc.find("bg-f3/content1", this.node);
     this.content2Node = cc.find("bg-f3/content2", this.node);
   },
   initData() {
     this.growNode.cascadeOpacity = false;
+    //初始化小鸡详情
     this.initChickData();
+    //初始化小鸡列表
+    this.initChickList();
   },
+  //初始化小鸡详情
   initChickData() {
     Func.GetChickValueById(this.Id).then(data => {
       if (data.Code === 1) {
@@ -38,19 +44,65 @@ cc.Class({
       }
     });
   },
+  //初始化小鸡列表
+  initChickList() {
+    //  获取正常的小鸡及已收取的小鸡
+    Func.GetChickList(3).then(data => {
+      if (data.Code === 1) {
+        for (let i = 0; i < data.List.length; i++) {
+          cc.loader.loadRes("Prefab/chickDetail/item", cc.Prefab, (err, prefab) => {
+            let itemNode = cc.instantiate(prefab);
+            let idLbael = cc.find("id", itemNode).getComponent(cc.Label);
+
+            idLbael.string = data.List[i].ID;
+            this.contentNode.addChild(itemNode);
+
+            itemNode.on("click", () => {
+              this.Id = data.List[i].ID;
+              this.initChickData();
+            });
+          });
+        }
+      }
+    });
+  },
+  //小鸡数据赋值
   assignChickData(data) {
     this.idLabel.string = `编号：${this.Id}`;
-    this.sexLabel.string = `性别：${data.Sex}`;
+    this.sexLabel.string = `性别：${data.Sex ? "小姐姐" : "小哥哥"}`;
     this.hungryLabel.string = `饥饿度：${data.StarvationValue}`;
     this.healthLabel.string = `健康值：${data.HealthValue}`;
-    this.collectButton.active = data.CallBack;
-    this.growProgressBar.progress = data.Proportion / 100;
-    this.growLabel.string = `${data.Proportion}/100`;
+
+    this.growProgressBar.progress = Math.round(data.Proportion) / 100;
+    this.growLabel.string = `${Math.round(data.Proportion)}/100`;
+
+    // 是否已被收取
+    if (data.CallBack) {
+      cc.loader.loadRes("chickDetail/collected", cc.SpriteFrame, (err, spriteFrame) => {
+        this.collectButton.getComponent(cc.Sprite).spriteFrame = spriteFrame;
+        this.collectButton.getComponent(cc.Button).interactable = false;
+      });
+    } else {
+      // 是否能被收取
+      if (data.ReturnBack) {
+        cc.loader.loadRes("chickDetail/collect", cc.SpriteFrame, (err, spriteFrame) => {
+          this.collectButton.getComponent(cc.Sprite).spriteFrame = spriteFrame;
+        });
+      } else {
+        cc.loader.loadRes("chickDetail/noCollect", cc.SpriteFrame, (err, spriteFrame) => {
+          this.collectButton.getComponent(cc.Sprite).spriteFrame = spriteFrame;
+          this.collectButton.getComponent(cc.Button).interactable = false;
+        });
+      }
+    }
   },
+  // 绑定事件
   bindEvent() {
+    //返回index页面
     this.backButton.on("click", () => {
       cc.director.loadScene("index");
     });
+    // 生长周期Tab
     this.growthButton.on("click", () => {
       this.growthLabelNode.color = cc.color("#FF4A4A");
       this.recordLabelNode.color = cc.color("#B2B2B2");
@@ -59,6 +111,7 @@ cc.Class({
       this.content1Node.active = true;
       this.content2Node.active = false;
     });
+    //转手记录Tab
     this.recordButton.on("click", () => {
       this.growthLabelNode.color = cc.color("#B2B2B2");
       this.recordLabelNode.color = cc.color("#FF4A4A");
