c07144d3f8f8b7ad675f6b0dd7be839da8ac2326
shockingsrose <847073376@qq.com>
2018-04-27 18:09:07 +0800

    1.修复产蛋棚 弹出的不是好友的产蛋棚信息

--- assets/Script/Data.js
+++ assets/Script/Data.js
@@ -554,7 +554,8 @@ var func = {
     });
   },
   // 获取产蛋棚坑位的信息
-  getEggLayInfo() {
+  getEggLayInfo(openID = this.openID) {
+
     return new Promise((resolve, reject) => {
       var xhr = new XMLHttpRequest();
       xhr.onreadystatechange = function() {
@@ -573,7 +574,7 @@ var func = {
         }
       };
       // POST方法
-      xhr.open('GET', Config.apiUrl + '/T_Base_LayEggsShed/GetPageList?openID=' + this.openID, true);
+      xhr.open('GET', Config.apiUrl + '/T_Base_LayEggsShed/GetPageList?openID=' + openID, true);
       xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded'); //缺少这句，后台无法获取参数
       xhr.send();
     });
