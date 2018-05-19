c83edb303174af604382ee38a82da643d9453ce8
chenyomi <408550179@qq.com>
2018-05-19 08:31:30 +0800

    更新

--- assets/Script/Config.js
+++ assets/Script/Config.js
@@ -1,6 +1,6 @@
 window.Config = {
   shopP2P: 1, //交易市场场景切换参数
-  apiUrl: 'http://www.jingongbao.com:4634',
+  apiUrl: 'http://wxapi.zjytny.cn',
   backUrl: 'userCenter',
   backIndexUrl: 'index',
   openID: null,
